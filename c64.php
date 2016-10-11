b15fc1560c6f9a21c957ac130ad9ef6e26ebe3d8
Joshua Thijssen <jthijssen@noxlogic.nl>
2016-10-11 20:58:31 +0200

    Load PRG files

--- c64.php
+++ c64.php
@@ -9,7 +9,21 @@ require_once "./vendor/autoload.php";
 date_default_timezone_set("Europe/Amsterdam");
 
 $c64 = new C64(new FileLogger("output.log"), new ShmIo());
-$c64->boot();
+$c64->boot(true);
+
+// Load PRG file if needed
+if ($argc == 2 && file_exists($argv[1]) && substr($argv[1], -4, 4) == ".prg") {
+    print "Loading PRG file: $argv[1]: ";
+    $c64->getMemory()->loadPrg($argv[1]);
+    print "done.\n";
+
+    // Type "RUN\n" so the program will automatically run
+    $c64->getMemory()->write8(0xC6, 4);
+    $c64->getMemory()->write8(0x277, 82);
+    $c64->getMemory()->write8(0x278, 85);
+    $c64->getMemory()->write8(0x279, 78);
+    $c64->getMemory()->write8(0x27A, 13);
+}
 
 // Loop de loop
 while (true) {
